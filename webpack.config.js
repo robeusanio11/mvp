59c8ce0ece07c4bcfe27213e8a00a6c8c9dcfbcf
robeusanio11 <robeusanio11@gmail.com>
2021-02-13 10:21:58 -0800

    repo setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'client/public')
+  },
+
+  plugins: [new webpack.ProgressPlugin()],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'client/src')],
+      loader: 'babel-loader'
+    }, {
+      test: /.css$/,
+
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  }
+}
